a8729453b38c7c12304991f4a5b566cd57efd036
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2025-09-06 19:44:16 +0200

    🦄 refactor(wheel.gd): rename variables to snake case, format file, prefix unused var

--- prefabs/wall_of_wheels/wheel.gd
+++ prefabs/wall_of_wheels/wheel.gd
@@ -12,23 +12,24 @@ class_name Wheel extends FreezeManagedRigidBody3D
 @export var material: StandardMaterial3D = StandardMaterial3D.new():
 	set(v):
 		material = v
-		if (_shapeMesh != null):
-			_shapeMesh.material = material
+		if (_shape_mesh != null):
+			_shape_mesh.material = material
+
 @export var radius: float:
 	set(v):
 		radius = v
 		_shape.radius = v
-		_shapeMesh.top_radius = v
-		_shapeMesh.bottom_radius = v
+		_shape_mesh.top_radius = v
+		_shape_mesh.bottom_radius = v
 		
 @export var height: float:
 	set(v):
 		height = v
 		_shape.height = v
-		_shapeMesh.height = v
+		_shape_mesh.height = v
 
 var _shape: CylinderShape3D = CylinderShape3D.new()
-var _shapeMesh: CylinderMesh = CylinderMesh.new()
+var _shape_mesh: CylinderMesh = CylinderMesh.new()
 
 func _ready() -> void:
 	freeze = true
@@ -39,17 +40,17 @@ func _ready() -> void:
 	collider.shape = _shape
 	var mesh: MeshInstance3D = MeshInstance3D.new()
 	add_child(mesh)
-	_shapeMesh.radial_segments = 16
-	mesh.mesh = _shapeMesh
-	_shapeMesh.material = material
+	_shape_mesh.radial_segments = 16
+	mesh.mesh = _shape_mesh
+	_shape_mesh.material = material
 	timeout.connect(func():
 		# when emitted, necessarily not frozen.
-		
-		# force the wheel to fall on the side so that
+		#
+		# Force the wheel to fall on the side so that
 		# it freezes by itself by stopping moving.
 		center_of_mass_mode = RigidBody3D.CENTER_OF_MASS_MODE_CUSTOM
 		center_of_mass = Vector3(0, -3 * height, 0)
 	)
-	freezeChanged.connect(func(frozen: bool):
+	freezeChanged.connect(func(_frozen: bool):
 		center_of_mass_mode = RigidBody3D.CENTER_OF_MASS_MODE_AUTO
 	)
