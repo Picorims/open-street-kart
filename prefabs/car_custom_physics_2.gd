1cb191994453e5b62a3a35a8b1ceb5c1a022a694
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2025-09-22 19:33:43 +0200

    🐞 fix(car): try to fix occasional crash on respawn

--- prefabs/car_custom_physics_2.gd
+++ prefabs/car_custom_physics_2.gd
@@ -52,7 +52,8 @@ const IS_STUCK_SPEED_SQUARED_THRESHOLD: float = 1
 # crash avoidance related constants
 const MIN_INERTIA_RADIUS_LIMIT = 0.001
 const MAX_INERTIA_RADIUS_LIMIT = 10_000
-const MIN_ANGLE_THRESHOLD = 0.01
+const MIN_YAW_THRESHOLD_FOR_CENTRIFUGAL_FORCE_COMPUTE = 0.03
+const MAX_LIN_VEL_FOR_CENTRIFUGAL_FORCE_COMPUTE = 100
 
 const SPEED_BOOST = 1.5
 
@@ -240,10 +241,12 @@ func _apply_single_wheel_suspension(suspension_ray: RayCast3D) -> void:
 # applied on x,z plan
 func _cancel_inertia(state: PhysicsDirectBodyState3D) -> void:
 	var radius: float = _get_radius_of_rotation(state)
+	if (radius == -1): # see doc for _get_radius_of_rotation()
+		return
 	# Projecting the linear velocity onto the forward / backward axis to know in which direction the car
 	# goes, assuming wheel adherence.
 	var forward_backward_lin_vel_direction: float = sign(state.linear_velocity.dot(global_basis.x))
-	var rotation_direction: float = sign(state.angular_velocity.y)
+	var rotation_direction: float = signf(state.angular_velocity.y)
 	var centrifugal_direction: Vector3 = global_basis.z.normalized() * rotation_direction * forward_backward_lin_vel_direction
 	if (centrifugal_direction.length() < 0.01):
 		return
@@ -255,15 +258,29 @@ func _cancel_inertia(state: PhysicsDirectBodyState3D) -> void:
 	state.apply_central_force(counter_force)
 	_debug_centrifugal_force = centrifugal_force
 
-# applied on x,z plan
+## If the rigid body is turning, it is following a circle with a center of attraction,
+## defining the centrifugal force. This function returns the radius of such circle based
+## on the angular velocity (on Y; yaw), and linear velocity. (Imagining an arc from origin
+## to new position after angular velocity is applied, with the arc length based on linear velocity,
+## the circle is bigger as the velocity increases)
+##
+## Returns -1 if the circle is too big and would cause infinite values (too small angular velocity,
+## or too big linear velocity)
+## 
+## Applied on x,z plan
 func _get_radius_of_rotation(state: PhysicsDirectBodyState3D) -> float:
 	# assuming a circle arc of length <previous pos to current pos>,
 	# of angle length equal to previous angular velocity,
 	# we compute the circumference of the entire circle,
 	# and deduct a radius from there
-	var length = (state.linear_velocity * Vector3(1, 0, 1)).length()
-	var angle = max(abs(state.angular_velocity.y), MIN_ANGLE_THRESHOLD) # avoid 0 to avoid division by zero crash
-	var circumference = (2 * PI / angle) * length
+	var length: float = (state.linear_velocity * Vector3(1, 0, 1)).length()
+	var yaw: float = absf(state.angular_velocity.y)
+ 	# avoid 0 to avoid division by zero crash
+	var yaw_too_small: bool = yaw < MIN_YAW_THRESHOLD_FOR_CENTRIFUGAL_FORCE_COMPUTE
+	var length_too_big: bool = length > MAX_LIN_VEL_FOR_CENTRIFUGAL_FORCE_COMPUTE
+	if (yaw_too_small or length_too_big):
+		return -1
+	var circumference = (2 * PI / yaw) * length
 	var radius = circumference / (2 * PI)
 	return radius
 
