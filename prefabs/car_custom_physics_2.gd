4c0ba59b583c8d4db686ddc48a563339dd469245
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2026-01-14 21:05:05 +0100

    🐞 fix(car): try reducing car physics related crashes

--- prefabs/car_custom_physics_2.gd
+++ prefabs/car_custom_physics_2.gd
@@ -236,6 +236,8 @@ func _apply_single_wheel_suspension(suspension_ray: RayCast3D) -> void:
 		var spring_force_vector: Vector3 = (spring_force - spring_damping_force) * spring_up_direction
 		
 		var force_position_offset = contact_point - global_position # at raycast collision point
+		if is_nan(force_position_offset.x) or is_nan(force_position_offset.y) or is_nan(force_position_offset.z):
+			return # avoid crash
 		apply_force(spring_force_vector, force_position_offset)
 
 
@@ -243,6 +245,8 @@ func _apply_single_wheel_suspension(suspension_ray: RayCast3D) -> void:
 func _cancel_inertia(state: PhysicsDirectBodyState3D) -> void:
 	var radius: float = _get_radius_of_rotation(state)
 	if (radius == -1): # see doc for _get_radius_of_rotation()
+		return
+	if (state.linear_velocity.length_squared() < 0.01):
 		return
 	# Projecting the linear velocity onto the forward / backward axis to know in which direction the car
 	# goes, assuming wheel adherence.
