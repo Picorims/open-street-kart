7d4d23f4b8357fa4691acc0e16b0f4b4e4d8de82
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2026-06-01 17:29:13 +0200

    ✨ feat(roads): preoperly handle T-intersection, handle N-gon intersections

--- scripts/road_linker.gd
+++ scripts/road_linker.gd
@@ -10,6 +10,7 @@ class_name RoadLinker extends Node3D
 
 signal linked
 const T_INTERSECT_SHIFT_DISTANCE_FACTOR: float = 2
+const MOVE_SAFETY_GAP := 0.5
 const RP_NEXT := RoadPoint.PointInit.NEXT
 const RP_PRIOR := RoadPoint.PointInit.PRIOR
 
@@ -46,6 +47,7 @@ var _t_intersections := 0
 var _intersections := 0
 var MIN_TICK_SLEEP_LINKING_S := 0.2
 var _loader: MapDataLoader
+var _road_manager: RoadManager
 
 func link_roads(road_manager: RoadManager, loader: MapDataLoader):
 	print("Identifying road nodes to link...")
@@ -56,6 +58,7 @@ func link_roads(road_manager: RoadManager, loader: MapDataLoader):
 	_attached_pairs = 0
 	_t_intersections = 0
 	_intersections = 0
+	_road_manager = road_manager
 	for container: Node3D in road_manager.get_children():
 		for container2: Node3D in road_manager.get_children():
 			if container == container2:
@@ -143,59 +146,14 @@ func _physics_process(delta: float) -> void:
 				_attached_pairs += 1
 			else:
 				print("Trying T-intersection: ", group)
-				# TODO
-				pass
+				_create_t_intersection(n0, n1)
 				_t_intersections += 1
-				# T intersection
-				#var continuous_p: RoadPoint
-				#var edge_p: RoadPoint
-				#if n0.get_prior_road_node() != null and n0.get_next_road_node() != null:
-					#continuous_p = n0
-					#edge_p = n1
-				#else:
-					#continuous_p = n1
-					#edge_p = n0
-				#
-				## first, move the edge out of the continuous road
-				#var move_axis: Vector3 = edge_p.transform.basis.x
-				#if n0.get_next_road_node() != null:
-					#move_axis *= -1
-				#var continuous_size: float = continuous_p.lane_width * continuous_p.lanes.size()
-				#continuous_size += continuous_p.shoulder_width_l + continuous_p.shoulder_width_r
-				#continuous_size += 2 * continuous_p.gutter_profile.length() # approximate, ignore projection
-				#var half_size := continuous_size / 2.0
-				#var offset_distance := half_size * T_INTERSECT_SHIFT_DISTANCE_FACTOR
-				#edge_p.position += move_axis * offset_distance
-				#
-				## second, split the continuous point into two side points
-				#continuous_p.disconnect_roadpoint(RoadPoint.PointInit.NEXT, RoadPoint.PointInit.PRIOR)
-				#continuous_p.disconnect_roadpoint(RoadPoint.PointInit.PRIOR, RoadPoint.PointInit.NEXT)
-				#var next_p = RoadPoint.new()
-				#var prior_p = RoadPoint.new()
-				#var next_p_sibling = continuous_p.get_next_road_node()
-				#var prior_p_sibling = continuous_p.get_prior_road_node()
-				#next_p.copy_settings_from(continuous_p, false)
-				#prior_p.copy_settings_from(continuous_p, false)
-				#var continuous_axis = continuous_p.basis.x
-				#next_p.position = continuous_p.position - continuous_axis * T_INTERSECT_SHIFT_DISTANCE_FACTOR
-				#prior_p.position = continuous_p.position + continuous_axis * T_INTERSECT_SHIFT_DISTANCE_FACTOR
-				#next_p.connect_roadpoint(RoadPoint.PointInit.NEXT, next_p_sibling, RoadPoint.PointInit.PRIOR)
-				#next_p.connect_roadpoint(RoadPoint.PointInit.PRIOR, next_p_sibling, RoadPoint.PointInit.NEXT)
-				#
-				## create intersection
-				#var intersection := RoadIntersection.new()
-				#intersection.settings = IntersectionNGon.new()
-				#intersection.add_branch(next_p)
-				#intersection.add_branch(prior_p)
-				#intersection.add_branch(edge_p)
-				#
-				#_loader.persist_in_current_scene(next_p)
-				#_loader.persist_in_current_scene(prior_p)
-				#_loader.persist_in_current_scene(intersection)
-		
 		elif group.size() > 2:
 			print("Trying intersection: ", group)
-			# TODO
+			var copy_arr: Array[RoadPoint] = []
+			for p: RoadPoint in group:
+				copy_arr.append(p)
+			_create_intersection(copy_arr)
 			_intersections += 1
 
 
@@ -217,3 +175,125 @@ func _connect_rp_cross_container(n0: RoadPoint, n1: RoadPoint):
 	n0.connect_container(from, n1, to)
 	n0.container.rebuild_segments()
 	n1.container.rebuild_segments()
+
+func _create_t_intersection(n0: RoadPoint, n1: RoadPoint):
+	var continuous_p: RoadPoint
+	var edge_p: RoadPoint
+	if n0.get_prior_road_node() != null and n0.get_next_road_node() != null:
+		continuous_p = n0
+		edge_p = n1
+	else:
+		continuous_p = n1
+		edge_p = n0
+	var continuous_p_pos := continuous_p.global_position
+	
+	# first, move the edge out of the continuous road
+	_move_rp_away(edge_p)
+	
+	# second, split the continuous point into two side points
+	var next_p_sibling := continuous_p.get_next_road_node()
+	var prior_p_sibling := continuous_p.get_prior_road_node()
+	continuous_p.disconnect_roadpoint(RoadPoint.PointInit.NEXT, RoadPoint.PointInit.PRIOR)
+	continuous_p.disconnect_roadpoint(RoadPoint.PointInit.PRIOR, RoadPoint.PointInit.NEXT)
+	var next_p = RoadPoint.new()
+	var prior_p = RoadPoint.new()
+	
+	var continuous_container: RoadContainer = continuous_p.get_parent()
+	continuous_container.add_child(prior_p)
+	_loader.persist_in_current_scene(prior_p)
+	
+	var road_container = RoadContainer.new()
+	road_container.density = continuous_container.density
+	road_container.collision_layer = continuous_container.collision_layer
+	road_container.collision_mask = continuous_container.collision_mask
+	road_container.use_lowpoly_preview = continuous_container.use_lowpoly_preview
+	_road_manager.add_child(road_container)
+	_loader.persist_in_current_scene(road_container)
+	road_container.add_child(next_p)
+	_loader.persist_in_current_scene(next_p)
+	# we need to split the road into two containers, to then
+	# be able to insert an intersection in between.
+	var p: RoadPoint = next_p_sibling
+	while p != null:
+		p.reparent(road_container)
+		p.container = road_container
+		p = p.get_next_road_node()
+	road_container.update_edges()
+	continuous_container.update_edges()
+	road_container.validate_edges()
+	continuous_container.validate_edges()
+	
+	next_p.copy_settings_from(continuous_p)
+	prior_p.copy_settings_from(continuous_p)
+	next_p.force_update_transform()
+	prior_p.force_update_transform()
+	next_p.global_position = continuous_p_pos
+	prior_p.global_position = continuous_p_pos
+	#var continuous_axis = continuous_p.basis.x
+	#next_p.position = continuous_p.position - continuous_axis * T_INTERSECT_SHIFT_DISTANCE_FACTOR
+	#prior_p.position = continuous_p.position + continuous_axis * T_INTERSECT_SHIFT_DISTANCE_FACTOR
+	next_p.connect_roadpoint(RoadPoint.PointInit.NEXT, next_p_sibling, RoadPoint.PointInit.PRIOR)
+	prior_p.connect_roadpoint(RoadPoint.PointInit.PRIOR, prior_p_sibling, RoadPoint.PointInit.NEXT)
+	_move_rp_away(next_p)
+	_move_rp_away(prior_p)
+	
+	# create intersection
+	_join_rps_into_intersection([next_p, edge_p, prior_p])
+	continuous_container.rebuild_segments()
+	road_container.rebuild_segments()
+	continuous_p.queue_free()
+
+## Assuming an edge RoadPoint, move it away on its linked axis.
+## Distance corresponds to the road's width. 
+func _move_rp_away(rp: RoadPoint) -> bool:
+	if rp.get_next_road_node() == null and rp.get_prior_road_node() == null:
+		push_warning("_move_rp_away(): cannot proceed, isolated point.")
+		return false
+	if rp.get_next_road_node() != null and rp.get_prior_road_node() != null:
+		push_error("_move_rp_away(): cannot proceed, not an edge RoadPoint.")
+		return false
+	var dir := 1
+	var sibling = rp.get_next_road_node()
+	if rp.get_next_road_node() == null:
+		dir = -1
+		sibling = rp.get_prior_road_node()
+	var width = rp.get_width_with_shoulders() + rp.gutter_profile.x * 2
+	if not sibling == null:
+		# avoid (or limit the risk that) the rp going beyond its sibling. 
+		width = min(width, rp.global_position.distance_to(sibling.global_position) - MOVE_SAFETY_GAP)
+		if width < 0:
+			width = 0
+	rp.global_position += rp.global_basis.z * dir * width
+	return true
+
+func _join_rps_into_intersection(rps: Array[RoadPoint]):
+	var container := RoadContainer.new()
+	_road_manager.add_child(container)
+	_loader.persist_in_current_scene(container)
+	var intersection := RoadIntersection.new()
+	intersection.settings = IntersectionNGon.new()
+	container.add_child(intersection)
+	_loader.persist_in_current_scene(intersection)
+	var avg_pos: Vector3 = Vector3.ZERO
+	var edges: Array[RoadPoint] = []
+	for p in rps:
+		avg_pos += p.global_position
+		var edge_rp := RoadPoint.new()
+		container.add_child(edge_rp)
+		_loader.persist_in_current_scene(edge_rp)
+		edge_rp.copy_settings_from(p)
+		edge_rp.global_position = p.global_position
+		edges.append(edge_rp)
+		if p.get_next_road_node() == null:
+			p.connect_container(RP_NEXT, edge_rp, RP_PRIOR)
+		else:
+			p.connect_container(RP_PRIOR, edge_rp, RP_NEXT)
+	avg_pos /= rps.size()
+	intersection.global_position = avg_pos
+	for e in edges:
+		intersection.add_branch(e)
+
+func _create_intersection(group: Array[RoadPoint]):
+	for p: RoadPoint in group:
+		_move_rp_away(p)
+	_join_rps_into_intersection(group)
