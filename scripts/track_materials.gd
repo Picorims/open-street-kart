7c49cc21b1b59cf49b9fe78d8d2a538706c4d0e9
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2026-07-17 16:31:16 +0200

    ✨ feat(terrain): add terrain materials, remove obsolete surface generation

---
+++ scripts/track_materials.gd
@@ -0,0 +1,17 @@
+# Open Street Kart is an arcade kart game where you race in real life areas reconstructed from Open Street Map
+# Copyright (c) 2025-2026 Charly Schmidt aka Picorims<picorims.contact@gmail.com> and Open Street Kart contributors
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+class_name TrackMaterials extends Resource
+
+@export var athletism_floor: String = "res://materials/environment/terrain/athletism_floor.tres"
+@export var bassin: String = "res://materials/environment/terrain/bassin_plastic.tres"
+@export var constructions_site_floor: String = "res://materials/environment/terrain/construction_site_floor.tres"
+@export var field_floor: String = "res://materials/environment/terrain/field_floor.tres"
+@export var forest_floor: String = "res://materials/environment/terrain/forest_floor.tres"
+@export var grass: String = "res://materials/environment/grass/grass_v2.tres"
+@export var scrub: String = "res://materials/environment/terrain/scrub.tres"
